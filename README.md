a6be7b91192e2603089a101bbe53088dc71f8389
sadnessOjisan <sadness.ojisan@gmail.com>
2023-05-25 16:45:33 +0900

    wip

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Nodejs でのモノレポのビルド後、main をどうするかについて�
 ## 論点
 
 - Docker で 1 サービスを展開する時、無駄なものを含めないようにするためにはどうすればいいか？
+- Node.js に ESM 読み込ませるなら tsconfig の設定どうしたらいいか. module, include, type...
--- packages/server/dist/main.js
+++ packages/server/dist/main.js
@@ -1,16 +1,11 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var fastify_1 = __importDefault(require("fastify"));
-var lib_a_1 = require("lib_a");
-var fastify = (0, fastify_1.default)({
+import Fastify from "fastify";
+import { helloWorld } from "lib_a";
+const fastify = Fastify({
     logger: true,
 });
 // Declare a route
 fastify.get("/", function (_, reply) {
-    (0, lib_a_1.helloWorld)();
+    helloWorld();
     reply.send({ hello: "world" });
 });
 // Run the server!
